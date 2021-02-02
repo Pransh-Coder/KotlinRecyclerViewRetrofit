6bab8f58ca968fbea58a2927a1c13422b2ef75be
Pransh Rastogi <rastogipransh4@gmail.com>
2021-02-02 12:14:36 +0530

    added imageview in recyclerView item

--- app/src/main/java/com/example/kotlinrecyclerviewretrofit/RecyclerAdapter.kt
+++ app/src/main/java/com/example/kotlinrecyclerviewretrofit/RecyclerAdapter.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 
 class RecyclerAdapter(private val context: Context,var photosList: ArrayList<Photos>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
@@ -15,6 +17,7 @@ class RecyclerAdapter(private val context: Context,var photosList: ArrayList<Pho
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.title.setText(photosList.get(position).title)
+        Picasso.get().load(photosList.get(position).thumbnailUrl).into(holder.imageView)
     }
 
     override fun getItemCount(): Int {
@@ -24,9 +27,11 @@ class RecyclerAdapter(private val context: Context,var photosList: ArrayList<Pho
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         var title:TextView
+        var imageView:ImageView
 
         init {
             title = itemView.findViewById(R.id.title)
+            imageView = itemView.findViewById(R.id.imageView)
         }
 
 
